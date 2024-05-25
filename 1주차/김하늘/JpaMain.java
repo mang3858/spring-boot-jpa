861416f232a8f71e74e32618a4d190c1c8a21c33
haneul <rlagksmf569@naver.com>
2024-05-26 00:49:15 +0900

    회원 등록

--- 1주차/김하늘/JpaMain.java
+++ 1주차/김하늘/JpaMain.java
@@ -10,7 +10,23 @@ public class JpaMain {
         EntityManager em = emf.createEntityManager();
         //code
 
-        em.close();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(3L);
+            member.setName("HelloC"); //프로젝트 실행 단축키 : Shift + F10
+
+            em.persist(member); //JPA에 저장하기
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
