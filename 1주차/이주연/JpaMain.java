aaa2e075d2d9e5b16b0d8dd12535848876dd23e3
jylee <juyeon01530@gmail.com>
2024-05-26 19:31:20 +0900

    프로젝트 생성

--- 1주차/이주연/JpaMain.java
+++ 1주차/이주연/JpaMain.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-        // 커밋 테스트
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         //code
