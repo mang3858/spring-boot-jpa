4cb0e87b8a2067d1d1f41ce8d00bad7619f0fc20
jylee <juyeon01530@gmail.com>
2024-05-27 03:44:14 +0900

    회원 등록

--- 1주차/이주연/JpaMain.java
+++ 1주차/이주연/JpaMain.java
@@ -5,12 +5,26 @@ import jakarta.persistence.*;
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+        // 트랜잭션 받아오기
+        EntityTransaction tx = em.getTransaction();
+        tx.begin(); // 트랜잭션 시작
+
+        try{
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member); // 멤버 저장
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
