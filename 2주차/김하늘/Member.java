73660838a4302baf7bfc49c2023f39996fa751ee
haneul <rlagksmf569@naver.com>
2024-06-02 00:57:02 +0900

    SEQUENCE 전략

--- 2주차/김하늘/Member.java
+++ 2주차/김하늘/Member.java
@@ -6,13 +6,14 @@ import java.util.Date;
 
 @Entity //얘가 있어야 JPA가 관리해야하는 애라고 인식함
 //@Table(name = "MBR") ; 테이블 명이 디비(MBR)와 코드(Member)가 다를 때
+@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
-  @Id //primary key 선언하기
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-  @Column(name = "name")
-  private String username;
+    @Id //primary key 선언하기
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq")
+    private Long id;
+    @Column(name = "name")
+    private String username;
 
   public Member() {
 
