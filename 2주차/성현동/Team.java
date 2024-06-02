8ac5c541b1db529d11eaf7fe84c66a357d07be52
hdsung98 <hdsung98@naver.com>
2024-06-03 01:50:59 +0900

    연관관계 맵핑 기초: 양방향 연관관계와 연관관계의 주인 - 다대일 관계에서 fk 있는 다 측이 주인, 일 측이 종속 - 다측에서 삽입,삭제 등 변경 가할수 있고, 일 측은 가짜맵핑한뒤 조회만 허용

--- 2주차/성현동/Team.java
+++ 2주차/성현동/Team.java
@@ -8,4 +8,7 @@ public class Team {
     @GeneratedValue
     private Long id;
     private String name;…
+
+    @OneToMany(mappedBy = "team") // db상에서 fk가 여기 없으므로 양방향관계의 주인x.(가짜맵핑)
+    List<Member> members = new ArrayList<Member>(); // 읽기전용
 }
