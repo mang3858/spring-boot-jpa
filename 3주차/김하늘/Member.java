e13cb800d0db88ee2b330693aa26f6c4ccb68969
haneul <rlagksmf569@naver.com>
2024-06-10 22:32:35 +0900

    일대일

--- 3주차/김하늘/Member.java
+++ 3주차/김하늘/Member.java
@@ -19,6 +19,10 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
 
