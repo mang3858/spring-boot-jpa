e10a361351ea11708fc6df150df6b177f192583c
Wish <80116126+mang3858@users.noreply.github.com>
2024-06-23 21:58:44 +0900

    10. 객체지향 쿼리 언어_기본문법

---
+++ 5주차/김소망/Member.java
@@ -0,0 +1,58 @@
+package jpal;
+
+import jakarta.persistence.*;
+
+import java.lang.management.MemoryType;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private  Team team;
+
+    @Enumerated(EnumType.STRING)
+    private MemoryType type;
+
+    public void changedTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
